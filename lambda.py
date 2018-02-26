d03a3fee18a297b6289e516580aa517ca724ea22
haukb <36861726+haukb@users.noreply.github.com>
2018-02-26 22:40:09 +0100

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -14,3 +14,4 @@ def evaluate_polynomial(a, x):
 evaluate_polynomial([1/factorial(x) for x in range(0, K)], X)
 # For K=10 and X=4, should print: 54.15414462081129
 
+#Question(Hauk): where to use reduce() func? 
