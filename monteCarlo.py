85bf040048f4d7721a20aca67870de9003c1a352
haukb <haukb@github.com>
2018-02-26 22:35:20 +0100

    Revert "H-Lambda"
    This reverts commit a63169ad0e3c0e66792d4f24e2c5dd7485067723.

--- monteCarlo.py
+++
@@ -1,9 +0,0 @@
-import random
-N = 100000
-
-x = [random.random() for a in range(N)]
-y = [random.random() for a in range(N)]
-
-pi = (4/N) * [(a**2 + b**2)<1 for a, b in zip(x,y)].count(1)
-
-print (pi)
